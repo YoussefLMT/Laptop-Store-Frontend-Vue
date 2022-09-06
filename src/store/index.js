5a1848c03dfb6d4d3ada40d689d18e2473de3fba
YoussefLMT <mcyoussef324@gmail.com>
2022-09-06 13:22:22 +0100

    add user state

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user:{
+        token: localStorage.getItem('token'),
+        role: localStorage.getItem('role')
+    }
   },
   getters: {
+    
   },
   mutations: {
+    
   },
   actions: {
+
   },
   modules: {
+
   }
 })
