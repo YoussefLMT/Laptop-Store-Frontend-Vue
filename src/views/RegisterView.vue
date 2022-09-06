6817a9364fdf04bf269091ca5d73a328911d25a3
YoussefLMT <mcyoussef324@gmail.com>
2022-09-06 13:09:37 +0100

    display errors and success messages

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -10,26 +10,31 @@
                 </div>
                 <div class="card-body">
                     <form>
+                        <div class="alert alert-success" v-if="message">
+                            {{ message }}
+                        </div>
                         <div class="row mb-3">
                             <div class="col-md-12">
                                 <div class="form-floating mb-3 mb-md-0">
                                     <input class="form-control" v-model="userData.name" id="name" type="text" placeholder="Enter your name" required />
                                     <label for="name">Name</label>
+                                    <span class="text-danger" v-if="errors.name">{{ errors.name[0] }}</span>
                                 </div>
                             </div>
                         </div>
                         <div class="form-floating mb-3">
                             <input class="form-control" v-model="userData.email" id="inputEmail" type="email" placeholder="name@example.com" required />
                             <label for="inputEmail">Email address</label>
+                            <span class="text-danger" v-if="errors.email">{{ errors.email[0] }}</span>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-12">
                                 <div class="form-floating mb-3 mb-md-0">
                                     <input class="form-control" v-model="userData.password" id="inputPassword" type="password" placeholder="Create a password" required />
                                     <label for="inputPassword">Password</label>
+                                    <span class="text-danger" v-if="errors.password">{{ errors.password[0] }}</span>
                                 </div>
                             </div>
-
                         </div>
                         <div class="mt-4 mb-0">
                             <div class="d-grid"><button type="button" @click="register" class="btn btn-primary btn-block">Create Account</button></div>
@@ -53,7 +58,7 @@ export default {
     components: {
         NavBar
     },
-     data() {
+    data() {
         return {
             userData: {
                 name: '',
@@ -64,15 +69,18 @@ export default {
             errors: ''
         }
     },
-    methods:{
-        async register(){
-             try {
-                const response = await axiosInstance.post("/register",this.userData)
+    methods: {
+        async register() {
+            try {
+                const response = await axiosInstance.post("/register", this.userData)
 
                 if (response.data.status === 200) {
-                    console.log(response.data.message)
+                    this.message = response.data.message
+                    this.userData.name = ''
+                    this.userData.email = ''
+                    this.userData.password = ''
                 } else {
-                    console.log(response.data.validation_err) 
+                    this.errors = response.data.validation_err
                 }
             } catch (error) {
                 console.log(error)
