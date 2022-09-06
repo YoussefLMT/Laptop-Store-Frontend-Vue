048cd82160a16d42f2d7975f520f422959561741
YoussefLMT <mcyoussef324@gmail.com>
2022-09-06 13:34:23 +0100

    add route for the dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/DashboardView.vue')
   }
 ]
 
