17f91fbddd61a87947b4d5cb0a897c4a0e8f74fc
YoussefLMT <mcyoussef324@gmail.com>
2022-09-07 13:08:22 +0100

    add home content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,68 @@
 <template>
-  <div class="home">
-    <NavBar/>
-  </div>
+<div class="home">
+    <NavBar />
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left-side">
+                <h2><span class="text-info">Lap</span>Tech</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Ab nostrum commodi vero voluptas sapiente facilis iste soluta
+                    maxime! Quaerat, sit odio? Totam accusamus provident ad eos dolorum
+                    fuga non id.
+                </p>
+                <button type="button" class="btn btn-primary">Shop Now</button>
+            </div>
+
+            <div class="col-md-6">
+                <div class="image">
+                    <img src="@/assets/laptop.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 
 export default {
-  name: 'HomeView',
-  components:{
-    NavBar,
-  }
+    name: 'HomeView',
+    components: {
+        NavBar,
+    }
 }
 </script>
+
+<style scoped>
+.image {
+    width: 480px;
+    margin-left: 90px;
+    margin-top: 50px;
+}
+
+.image img {
+    width: 100%;
+}
+
+.left-side{
+  margin-top: 150px;
+}
+
+h2 {
+    font-weight: bold;
+    font-size: 50px;
+}
+
+p{
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+.btn-primary{
+  border-radius: 20px;
+  font-weight: bold;
+  margin-top: 15px;
+}
+</style>
