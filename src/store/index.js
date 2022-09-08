0fcb219b64a8b98917c8dfe98386f46e16cbe7d8
YoussefLMT <mcyoussef324@gmail.com>
2022-09-08 18:53:50 +0100

    add products state, mutations, actions and getters

--- src/store/index.js
+++ src/store/index.js
@@ -5,18 +5,43 @@ export default createStore({
     user:{
         token: localStorage.getItem('token'),
         role: localStorage.getItem('role')
-    }
+    },
+    products: [],
+    loading: false
   },
   getters: {
-    
+    products(state) {
+      return state.products
+    },
+
+    loading(state) {
+      return state.loading
+    }
   },
   mutations: {
     setUserRole(state, role){
       state.user.role = role
+    },
+
+    setLoading(state, loading) {
+      state.loading = loading
+    },
+
+    getProducts(state, products) {
+      state.products = products
     }
   },
   actions: {
+    getProducts({ commit }) {
+      commit('setLoading', true)
 
+      axiosInstance.get('/products')
+        .then(response => {
+          commit('getProducts', response.data.products)
+          commit('setLoading', false)
+        })
+        .catch(error => console.log(error))
+    }
   },
   modules: {
 
