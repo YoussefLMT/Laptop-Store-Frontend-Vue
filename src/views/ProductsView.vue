3588f0498bd02700795eefbaa766a14a3f1fe823
YoussefLMT <mcyoussef324@gmail.com>
2022-09-08 13:01:39 +0100

    add sidebar to products view

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,16 +1,32 @@
 <template>
-    <div>
+<SidebarMenu :menu="menu" />
+
+    <main>
         <h1>Products</h1>
-    </div>
+    </main>
 
 </template>
 
 <script>
+import menu from '../components/menu'
+import { SidebarMenu } from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+
 export default {
-    
+    components:{
+        SidebarMenu
+    },
+    data(){
+        return{
+            menu
+        }
+    }
 }
 </script>
 
 <style scoped>
-    
+main {
+    margin-left: 300px;
+    padding: 20px;
+} 
 </style>
