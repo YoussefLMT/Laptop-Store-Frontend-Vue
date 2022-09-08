de99e6f4deebd9a81d73cdaa52a4801442f1bcd0
YoussefLMT <mcyoussef324@gmail.com>
2022-09-08 18:18:38 +0100

    add profucts table

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -3,6 +3,38 @@
 
     <main>
         <h1>Products</h1>
+         <div class="card products" style="width: 1000px;">
+            <div class="card-header">
+                Products Managment
+
+               
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">name</th>
+                            <th scope="col">price</th>
+                            <th scope="col">quantity</th>
+                            <th scope="col">description</th>
+                            <th scope="col">image</th>
+                            <th scope="col">actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">test</th>
+                            <td>test</td>
+                            <td>test</td>
+                            <td>test</td>
+                            <td>test</td>
+                            <td><img src="@/assets/laptop.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 
 </template>
@@ -29,4 +61,16 @@ main {
     margin-left: 300px;
     padding: 20px;
 } 
+
+.products {
+    margin: 50px auto;
+}
+
+.btn-add {
+    float: right
+}
+
+img{
+    width: 50px;
+}
 </style>
