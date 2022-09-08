4be9f2677f03ce2b932969a211ca0b3d29fd18d0
YoussefLMT <mcyoussef324@gmail.com>
2022-09-08 18:13:13 +0100

    improve product card design

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,11 +1,14 @@
 <template>
         <div class="row">
-            <div class="col-12 col-md-6 col-lg-4">
+            <div class="col-md-4">
                 <div class="card" style="width: 15rem;">
-                    <img class="card-img-top tt" src="@/assets/laptop.png">
+                    <img class="card-img-top w-100" src="@/assets/laptop.png">
                     <div class="card-body">
                         <h5 class="card-title">test</h5>
+                        <div class="d-flex justify-content-between">
                         <p class="card-text">test</p>
+                        <p class="card-text mr-4 text-success">100$</p>
+                        </div>
                         <a href="/#" class="btn btn-primary">Show Product</a>
                     </div>
                 </div>
@@ -18,8 +21,8 @@ export default {
 }
 </script>
 <style scoped>
-.tt{
+/* .tt{
    width: 230px;
    height: 230px;
-}
+} */
 </style>
