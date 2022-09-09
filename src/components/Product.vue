f484cd149405ff0e92c19184c7199f93895836d7
YoussefLMT <mcyoussef324@gmail.com>
2022-09-09 11:43:05 +0100

    add props to product component

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,28 +1,27 @@
 <template>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card" style="width: 15rem;">
-                    <img class="card-img-top w-100" src="@/assets/laptop.png">
-                    <div class="card-body">
-                        <h5 class="card-title">test</h5>
-                        <div class="d-flex justify-content-between">
-                        <p class="card-text">test</p>
-                        <p class="card-text mr-4 text-success">100$</p>
-                        </div>
-                        <a href="/#" class="btn btn-primary">Show Product</a>
-                    </div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="card" style="width: 15rem;">
+            <img class="card-img-top w-100" :src="image">
+            <div class="card-body">
+                <h5 class="card-title">{{ name }}</h5>
+                <div class="d-flex justify-content-between">
+                    <p class="card-text">{{ description }}</p>
+                    <p class="card-text mr-4 text-success">{{ price }}$</p>
                 </div>
+                <a href="/#" class="btn btn-primary">Show Product</a>
             </div>
         </div>
+    </div>
+</div>
 </template>
+
 <script>
 export default {
-    
+    props: ['image', 'name', 'description', 'price'],
 }
 </script>
+
 <style scoped>
-/* .tt{
-   width: 230px;
-   height: 230px;
-} */
+
 </style>
