6fe41fbd6763e2ef1d2337a1d68f169568231584
YoussefLMT <mcyoussef324@gmail.com>
2022-09-09 21:51:40 +0100

    add update product form and data

--- src/views/UpdateProductView.vue
+++ src/views/UpdateProductView.vue
@@ -1,8 +1,33 @@
 <template>
 <SidebarMenu :menu="menu" @update:collapsed="onToggleCollapse" />
 <main>
-    <h1>update product</h1>
-
+    <h3>Update Product</h3>
+    <div class="card pro" style="width: 800px;">
+            <div class="card-header">
+                Products Managment
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="fname" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="fname" v-model="product.name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="lname" class="form-label">Price</label>
+                        <input type="text" class="form-control" id="lname" v-model="product.price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Quantity</label>
+                        <input type="text" class="form-control" id="email" v-model="product.quantity">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea class="form-control" id="description" rows="3" v-model="product.description"></textarea>
+                    </div>
+                    <button type="button" class="btn btn-primary">Update Product</button>
+                </form>
+            </div>
+        </div>
 </main>
 </template>
 
@@ -22,6 +47,13 @@ components: {
         return {
             menu,
             collapsed: false,
+            product: {
+                name: '',
+                price: '',
+                quantity: '',
+                description: '',
+                image: ''
+            },
         }
     },
     methods:{
@@ -41,6 +73,10 @@ main {
     margin-left: 300px;
     padding: 20px;
     transition: .3s ease-out;
+}
+
+.pro {
+    margin: 20px auto;
 }
 
 .mll {
