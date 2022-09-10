b6f4c2c3ec5d8df4304454c65bfd53c5928c5403
YoussefLMT <mcyoussef324@gmail.com>
2022-09-10 20:29:09 +0100

    add users table

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -3,6 +3,33 @@
 
 <main :class="{ 'mll': collapsed }">
     <h1>Users</h1>
+      <div class="card users" style="width: 900px;">
+        <div class="card-header">
+            Users Managment
+
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">email</th>
+                        <th scope="col">role</th>
+                        <th scope="col">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr >
+                        <th scope="row">77</th>
+                        <td>yy</td>
+                        <td>uu</td>
+                        <td>yy</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 </main>
 </template>
@@ -41,6 +68,10 @@ export default {
 main {
     margin-left: 300px;
     padding: 20px;
+}
+
+.users{
+    margin: 50px auto;
 }
 
 .mll {
