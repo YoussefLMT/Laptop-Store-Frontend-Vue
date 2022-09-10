7dae04c74f6588abfdc4e61af085f91955646444
YoussefLMT <mcyoussef324@gmail.com>
2022-09-10 20:22:18 +0100

    add users view

---
+++ src/views/UsersView.vue
@@ -0,0 +1,31 @@
+<template>
+<SidebarMenu :menu="menu" @update:collapsed="onToggleCollapse" />
+
+    <main :class="{ 'mll': collapsed }">
+        <h1>Users</h1>
+
+    </main>
+</template>
+
+<script>
+import menu from '../components/menu'
+import {
+    SidebarMenu
+} from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+
+export default {
+    components: {
+        SidebarMenu
+    },
+    data(){
+        return{
+            menu
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
