f45d0ea9f79e4bc95e17eb3fd3ffccc6ec1b4ac0
YoussefLMT <mcyoussef324@gmail.com>
2022-09-10 12:33:04 +0100

    add product component and call getProducts method

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,8 +1,22 @@
-<template >
-    <div class="shop">
-            <NavBar />
+<template>
+<div class="shop">
+    <NavBar />
+    <div class="container mt-5">
+        <h2 class="text-center">All Our Products</h2>
+        <div class="row">
+            <div v-if="loading" class="text-center mb-5">
+                <h3>Loading...</h3>
+            </div>
 
+            <div class="row" v-else>
+                <div class="col-md-3 mb-5" v-for="product in homeProducts" :key="product.id">
+                    <Product :image="'http://127.0.0.1:8000/' + product.image" :name="product.name" :price="product.price" :description="product.description" :stock="product.quantity" />
+                </div>
+            </div>
+        </div>
     </div>
+
+</div>
 </template>
 
 <script>
@@ -15,9 +29,20 @@ export default {
         NavBar,
         Product
     },
+    mounted() {
+        store.dispatch('getProducts')
+    },
+     computed: {
+        products() {
+            return store.getters.products
+        },
+        loading() {
+            return store.getters.loading
+        }
+    },
 }
 </script>
 
 <style >
-    
+
 </style>
