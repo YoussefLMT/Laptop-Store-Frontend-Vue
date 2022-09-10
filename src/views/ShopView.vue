4bac62261c1c8f591b541b04a2bad2f2b0a29eed
YoussefLMT <mcyoussef324@gmail.com>
2022-09-10 12:28:20 +0100

    add shop view

---
+++ src/views/ShopView.vue
@@ -0,0 +1,23 @@
+<template >
+    <div class="shop">
+            <NavBar />
+
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Product from '@/components/Product.vue'
+import store from '@/store'
+
+export default {
+    components: {
+        NavBar,
+        Product
+    },
+}
+</script>
+
+<style >
+    
+</style>
