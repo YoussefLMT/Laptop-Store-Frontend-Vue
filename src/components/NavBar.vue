3029d6c511cf29a530be3063d1035e852c328e1c
YoussefLMT <mcyoussef324@gmail.com>
2022-09-11 12:19:47 +0100

    improve contact view ui

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -23,7 +23,7 @@
                         <router-link class="nav-link" to="/about">About</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/#">Contact</router-link>
+                        <router-link class="nav-link" to="/contact">Contact</router-link>
                     </li>
 
                     <div v-if="!this.$store.state.user.token" class="d-flex">
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,25 +1,31 @@
 <template>
 <NavBar />
-<div style="width: 75%; min-height: 450px;">
-    <div class="w3-container" id="contact">
-        <h2>Contact us</h2>
-        <p>If you have any questions, do not hesitate to ask them.</p>
-        <i class="fa fa-map-marker w3-text-blue" style="width: 30px"></i>
-        Chicago, US<br> <i class="fa fa-phone w3-text-blue" style="width: 30px"></i> Phone: +00 151515<br> <i class="fa fa-envelope w3-text-blue" style="width: 30px"> </i> Email:
-        mail@mail.com<br>
-        <form action="../SendMessageByUser" method="post">
-            <input type="text" name="current_user" value='<%= session.getAttribute( "Username" ) %>' style="display: none;">
-            <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
-            <p>
-                <input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message">
-            </p>
-            <p>
-                <button class="w3-button w3-black w3-padding-large" type="submit">SEND
-                    MESSAGE</button>
-            </p>
-        </form>
+<div class="container">
+    <div class="row">
+        <div class="contact">
+            <div class="w3-container" id="contact">
+                <h2 class="text-center mb-3">Contact us</h2>
+                <p>If you have any questions, do not hesitate to ask them.</p>
+                <i class="fa fa-map-marker w3-text-blue mb-2" style="width: 30px"></i>
+                Chicago, US<br>
+                <i class="fa fa-phone w3-text-blue mb-2" style="width: 30px"></i>
+                Phone: +00 151515<br><i class="fa fa-envelope w3-text-blue mb-3" style="width: 30px"> </i>
+                Email: mail@mail.com<br>
+                <form>
+                    <input type="text" name="current_user" value='<%= session.getAttribute( "Username" ) %>' style="display: none;">
+                    <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
+                    <p>
+                        <input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message">
+                    </p>
+                    <p>
+                        <button class="w3-button w3-black w3-padding-large" type="button">SEND MESSAGE</button>
+                    </p>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
+
 <Footer />
 </template>
 
@@ -36,5 +42,12 @@ export default {
 </script>
 
 <style scoped>
-
+.contact {
+    margin: 0 auto;
+    margin-top: 90px;
+    padding: 20px;
+    border-radius: 10px;
+    width: 600px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
 </style>
