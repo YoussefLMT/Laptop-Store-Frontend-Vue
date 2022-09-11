4bb817142de878fc70f9d573f58063336b434341
YoussefLMT <mcyoussef324@gmail.com>
2022-09-11 19:42:36 +0100

    call cart products action

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,26 +1,37 @@
 <template>
 <NavBar />
 
-    <div class="container mt-5">
-        <h1>Cart</h1>
-        
-    </div>
-    <Footer />
+<div class="container mt-5">
+    <h1>Cart</h1>
 
+</div>
+<Footer />
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
+import store from '@/store'
 
 export default {
     components: {
         NavBar,
         Footer
     },
+    mounted() {
+        store.dispatch('getCartProducts')
+    },
+     computed: {
+        cartProducts() {
+            return store.getters.cartProducts
+        },
+        loading() {
+            return store.getters.loading
+        }
+    },
 }
 </script>
 
 <style scoped>
-    
+
 </style>
