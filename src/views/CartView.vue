522c59228ffa4685bc061daa1459e7c203c2781a
YoussefLMT <mcyoussef324@gmail.com>
2022-09-12 18:41:12 +0100

    display image and text if the cart is empty

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -9,9 +9,19 @@
     </div>
 
     <div v-else>
-        <a href="/#" class="btn btn-primary">Order Now</a><br><br>
+        <div v-if="cartProductsLenth === 0" class="empty-cart mt-5">
+            <div class="row">
+                <div class="col-md-">
+                    <img src="@/assets/empty_cart.png">
+                    <h3>Your cart is currently empty</h3>
+                    <router-link to="/shop" class="btn btn-warning mt-3">Return To Shop</router-link>
+                </div>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-12 col-md-6 col-lg-4" v-for="product in cartProducts" :key="product.id">
+                <a href="/#" class="btn btn-primary">Order Now</a><br><br>
 
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" :src="'http://127.0.0.1:8000/' + product.image">
@@ -47,6 +57,9 @@ export default {
     computed: {
         cartProducts() {
             return store.getters.cartProducts
+        },
+        cartProductsLenth() {
+            return this.cartProducts.length
         },
         loading() {
             return store.getters.loading
@@ -91,5 +104,12 @@ export default {
 </script>
 
 <style scoped>
+.empty-cart {
+    width: 300px;
+    margin: 0 auto;
+}
 
+.empty-cart img {
+    width: 100%;
+}
 </style>
