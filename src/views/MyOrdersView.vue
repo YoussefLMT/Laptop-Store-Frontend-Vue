1c74fb8d5851f31fa8a44c930d7ff02d65bfa692
YoussefLMT <mcyoussef324@gmail.com>
2022-09-13 19:40:15 +0100

    display orders

--- src/views/MyOrdersView.vue
+++ src/views/MyOrdersView.vue
@@ -3,20 +3,23 @@
 
 <div class="container mt-5">
     <h3>My Orders</h3><br><br>
-    <div class="row">
+    <div v-if="loading" class="text-center mb-5">
+        <h3>Loading...</h3>
+    </div>
+    <div class="row" v-else v-for="order in orders" :key="order.id">
         <div class="col-sm-3">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="@/assets/laptop.png">
+                <img class="card-img-top" :src="'http://127.0.0.1:8000/' + order.image">
                 <div class="card-body">
-                    <h5 class="card-title">test</h5>
-                    <p class="card-text">300$</p>
+                    <h5 class="card-title">{{ order.name }}</h5>
+                    <p class="card-text">{{ order.price }}$</p>
                 </div>
             </div>
         </div>
         <div class="col-sm-4 ml-5">
-            <p>address: test</p>
-            <p>city: test</p>
-            <p>Status: test</p>
+            <p>Address: {{ order.address }}</p>
+            <p>City: {{ order.city }}</p>
+            <p>Status: {{ order.status }}</p>
         </div>
     </div>
 </div>
@@ -27,11 +30,23 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
+import store from '@/store'
 
 export default {
     components: {
         NavBar,
         Footer
+    },
+    mounted() {
+        store.dispatch('getOrders')
+    },
+    computed: {
+        orders() {
+            return store.getters.orders
+        },
+        loading() {
+            return store.getters.loading
+        }
     },
 }
 </script>
