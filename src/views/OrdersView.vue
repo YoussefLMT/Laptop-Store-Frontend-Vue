969ad6e1bd93baba1dd2418d903271028f632784
YoussefLMT <mcyoussef324@gmail.com>
2022-09-14 19:55:07 +0100

    add orders view

---
+++ src/views/OrdersView.vue
@@ -0,0 +1,47 @@
+<template>
+<SidebarMenu :menu="menu" @update:collapsed="onToggleCollapse" />
+
+<main :class="{ 'mll': collapsed }">
+</main>
+</template>
+
+<script>
+import menu from '../components/menu'
+import {
+    SidebarMenu
+} from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+
+export default {
+    components: {
+        SidebarMenu
+    },
+    data() {
+        return {
+            menu,
+            collapsed: false,
+        }
+    },
+    methods:{
+        onToggleCollapse(collapsed) {
+            if (collapsed) {
+                this.collapsed = true
+            } else {
+                this.collapsed = false
+            }
+        },
+    }
+}
+</script>
+
+<style scoped>
+main {
+    margin-left: 300px;
+    padding: 20px;
+    transition: .3s ease-out;
+}
+
+.mll {
+    margin-left: 70px;
+}
+</style>
