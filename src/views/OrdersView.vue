b6ae70638e39a15f730a3bdaf0711cdea69204da
YoussefLMT <mcyoussef324@gmail.com>
2022-09-14 20:30:36 +0100

    add order product modal

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -41,6 +41,29 @@
         </div>
     </div>
 
+    <!--Order Products Modal -->
+    <div class="modal fade" id="getOrderProducts" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Order Products</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card" style="width: 18rem;">
+                        <img src="@/assets/laptop.png" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 </template>
 
@@ -98,5 +121,10 @@ main {
 
 .orders {
     margin: 50px auto;
+}
+
+img{
+    width: 150px;
+    margin: 0 auto;
 }
 </style>
